5c04a3ad1b7d1d7a3a74b99f6ba5f166635eaf79
Emmanuel Norambuena <eanorambuena@uc.cl>
2026-06-14 01:07:05 -0400

    feat: right-click edge to delete connection

--- components/pipeline/PipelineEditor.vue
+++ components/pipeline/PipelineEditor.vue
@@ -57,6 +57,7 @@
         fit-view-on-init
         :node-types="nodeTypes"
         @connect="onConnect"
+        @edge-context-menu="onEdgeContextMenu"
       >
         <Background :gap="20" pattern-color="#2a2a2a" />
         <Controls position="bottom-right" />
@@ -125,6 +126,10 @@ function onConnect(connection: any) {
     id: `e-${connection.source}-${connection.target}`,
     style: { stroke: '#555', strokeWidth: 2 },
   }]
+}
+
+function onEdgeContextMenu(edge: any) {
+  edges.value = edges.value.filter(e => e.id !== edge.id)
 }
 
 function formatResult(val: any): string {
