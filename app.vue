69af7ecd394136ec46fac0b01758730f2984a705
Emmanuel Norambuena <eanorambuena@uc.cl>
2026-06-13 23:24:40 -0400

    fix: add NuxtPage :key=route.path to force page refresh on navigation

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <NuxtPage :key="route.path" />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+watch(() => route.path, () => {
+  if (import.meta.client) window.scrollTo(0, 0)
+})
+</script>
