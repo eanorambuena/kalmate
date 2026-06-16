ae89622f3e0b1b4333a3e36e88023678bbedac68
Emmanuel Norambuena <eanorambuena@uc.cl>
2026-06-16 01:46:27 -0400

    fix: position handles relative to each flex row (relative + top:50%) with 12px padding from labels

--- components/pipeline/nodes/CustomNode.vue
+++ components/pipeline/nodes/CustomNode.vue
@@ -89,22 +89,22 @@
     <div class="flex items-start justify-between gap-1 mb-1">
       <div class="flex flex-col gap-1">
         <template v-if="def.type === 'portfolioInput'">
-          <div v-for="(weight, idx) in data.weights" :key="'in-' + idx" class="flex items-center gap-1">
-            <Handle type="target" :position="Position.Left" :id="`in${idx}`" class="w-2 h-2 !bg-[#2979ff] !border-0" :style="{ position: 'absolute', left: '0px', top: `${20 + idx * 24}px` }" />
+          <div v-for="(weight, idx) in data.weights" :key="'in-' + idx" class="flex items-center gap-1 relative pl-3">
+            <Handle type="target" :position="Position.Left" :id="`in${idx}`" class="w-2 h-2 !bg-[#2979ff] !border-0" :style="{ position:'absolute', left:'0px', top:'50%' }" />
             <span class="text-[9px] text-[#bbb]">In{{ idx + 1 }}</span>
           </div>
         </template>
         <template v-else>
-          <div v-for="(inp, i) in def.inputs" :key="'in-' + i" class="flex items-center gap-1">
-            <Handle type="target" :position="Position.Left" :id="inp.id" class="w-2 h-2 !bg-[#2979ff] !border-0" :style="{ position: 'absolute', left: '0px', top: `${20 + i * 24}px` }" />
+          <div v-for="(inp, i) in def.inputs" :key="'in-' + i" class="flex items-center gap-1 relative pl-3">
+            <Handle type="target" :position="Position.Left" :id="inp.id" class="w-2 h-2 !bg-[#2979ff] !border-0" :style="{ position:'absolute', left:'0px', top:'50%' }" />
             <span class="text-[9px] text-[#bbb]">{{ inp.label }}</span>
           </div>
         </template>
       </div>
       <div class="flex flex-col gap-1">
-        <div v-for="(out, i) in def.outputs" :key="'out-' + i" class="flex items-center gap-1">
+        <div v-for="(out, i) in def.outputs" :key="'out-' + i" class="flex items-center gap-1 relative pr-3">
           <span class="text-[9px] text-[#bbb]">{{ out.label }}</span>
-            <Handle type="source" :position="Position.Right" :id="out.id" class="w-2 h-2 !bg-[#00c853] !border-0" :style="{ position: 'absolute', right: '0px', top: `${20 + i * 24}px` }" />
+          <Handle type="source" :position="Position.Right" :id="out.id" class="w-2 h-2 !bg-[#00c853] !border-0" :style="{ position:'absolute', right:'0px', top:'50%' }" />
         </div>
       </div>
     </div>
